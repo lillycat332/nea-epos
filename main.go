b5752fed583c2fa8ba9cb22a155f5ba3a72a64c1
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-23 14:45:39 +0100

    update makefile

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, `servepls is a simple webserver.
-		Launching this will activate the webserver on port :8080 or a port specified by -port.
-		For more information see the documentation.
-		Usage:
-		`)
+		fmt.Fprintf(os.Stderr, `
+servepls is a simple webserver.
+Launching this will activate the webserver on port :8080 or a port specified by -port.
+For more information see the documentation.
+Usage:\n`)
 		flag.PrintDefaults()
 	}
 }
