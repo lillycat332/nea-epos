f6478f9fddb9c6ef044532583469d9915cfc427e
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-28 14:13:03 +0100

    Added authentication

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,16 +1,65 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// loginHandler handles the /login form input and writes the response to the client (Login failed/successful)
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	log.Printf("POST request (Login) recieved from %s", r.RemoteAddr)
-	log.Fatal("I haven't implemented this yet!")
+	log.Printf("Login request recieved from %s", r.RemoteAddr)
+	userName := r.FormValue("username")
+	unhashedPassword := r.FormValue("password")
+	if testPass(userName, unhashedPassword) {
+		w.WriteHeader(http.StatusOK)
+		enableCors(&w)
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, "Login Successful")
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		enableCors(&w)
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, "Login Failed")
+	}
+}
+
+// TestPass tests the password against the database and returns true if the password is correct
+func testPass(userName string, unhashedPassword string) bool {
+	rdb, _ := sql.Open("sqlite3", db)
+	defer rdb.Close()
+	u := authQueryUser(rdb, userName)
+	passIsValid := false
+	if checkPasswordMatch(u[1], unhashedPassword) {
+		passIsValid = true
+	}
+	return passIsValid
+}
+
+// authQueryUser performs an SQL query to get the user's hashed password
+func authQueryUser(db *sql.DB, userName string) []string {
+	var users []string
+	rows, err := db.Query("SELECT username, password FROM users WHERE username = ?", userName)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		users = append(users, username)
+	}
+	return users
 }
